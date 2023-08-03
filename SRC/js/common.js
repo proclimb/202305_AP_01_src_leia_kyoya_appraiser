c3f74ec02dabf3ebf03bb3ba8585994e1752d2d8
leia kyoya <202305ap01_kyoya@x-ide.sakura.ne.jp>
2023-08-03 10:46:23 +0900

    バグ #14399
    【不具合状況の説明】
    
    仕入登録時、内見の項目を入力していない場合、
    必須項目では「正しい内見日付を入力してください」というメッセージが表示されるという事象が発生
    
    【修正内容の説明】
    該当箇所の修正を行い、改善致しました。

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -11,13 +11,24 @@ function fnYMDCheck(msg, obj) {
 	var tmp = oYMD.split('/');
 	var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
 	var vYMD = ymd.getFullYear() + '/' + ('0' + (ymd.getMonth() + 1)).slice(-2) + '/' + ('0' + ymd.getDate()).slice(-2);
-	if (oYMD == vYMD) {
-		return true;
+	// if (oYMD == vYMD) {
+	// 	return true;
+	// } else {
+	// 	alert(msg + "を入力してください");
+	// 	return false;
+	// }
+	if (oYMD.length) {
+		if (oYMD == vYMD) {
+			return true;
+		} else {
+			alert(msg + "を入力してください");
+			return false;
+		}
 	} else {
-		alert(msg + "を入力してください");
-		return false;
+		return true;
 	}
 }
+
 
 
 
